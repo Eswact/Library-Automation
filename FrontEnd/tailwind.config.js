e69e4241627002496bfd23ae2b9c53d953e9116e
Eren Sarıbay <eren99srb@gmail.com>
2024-03-24 16:48:57 +0300

    tailwindCss-added

---
+++ FrontEnd/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'main': '#1C424F',
+        'second': '#EE605F',
+        'dark': '#181818',
+        'main-shadow': '#1c424f5e',
+        'second-shadow': '#ee5d5d5e',
+      },
+    },
+    screens: {
+      'full': {'max': '1919px'},
+      '2xl': {'max': '1600px'},
+      'xl': {'max': '1280px'},
+      'lg': {'max': '1000px'},
+      'md': {'max': '850px'},
+      'sm': {'max': '600px'},
+    }
+  },
+  plugins: [],
+}
+
